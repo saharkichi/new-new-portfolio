63d10b047aafcdaf0e558273b56ea0e247f28c69
saharkichi <saharkichi@gmail.com>
2022-07-25 18:34:42 -0400

    import all component folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './App.css';
+import aboutme from "./components/aboutme";
+import contact from "./components/contact";
+import footer from "./components/footer";
+import header from "./components/header";
+import homepage from "./components/homepage";
+import navbar from "./components/navbar";
+import portfolio from "./components/portfolio";
+import projects from "./components/projects";
 
 function App() {
   
