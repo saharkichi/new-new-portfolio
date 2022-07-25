02bbb10b51e4e8cf2ecd19c375f3005be1ed13f0
saharkichi <saharkichi@gmail.com>
2022-07-25 19:19:49 -0400

    contact.js updated

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,1 +1,3 @@
 //WHEN I am presented with the About Me section THEN I see a recent photo or avatar of the developer and a short bio about them
+
+//  My name is Sahar Kichi! I recently moved downtown Toronto in 2021 after living in Burlington my entire life! I am a complete newbie when it comes to web development and coding after deciding to choose this career choice over my orginial plan of law school. I recently graduated from Ryerson University with a Bachelor of Arts in Criminology and looking forward to complete my program at the University of Toronto.
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,1 +1,25 @@
 // WHEN I am presented with the Contact section THEN I see a contact form with fields for a name, an email address, and a message WHEN I move my cursor out of one of the form fields without entering text THEN I receive a notification that this field is required WHEN I enter text into the email address field THEN I receive a notification if I have entered an invalid email address
+import React from 'react';
+import "./components.css";
+
+function contact() {
+    return (
+        <div>
+            <h1> Contact Me!</h1>
+            <p>
+                <form>
+                    <input type="text" placeholder="Enter name" />
+                    <br>
+                        <input type="email" placeholder="Enter your email" />
+                    </br>
+                        <textarea cols="60" rows="30" placeholder="Leave a message"></textarea>
+                    <br>
+                        <button type="submit"></button>
+                    </br>
+                </form>
+            </p>
+        </div>     
+    )
+}
+
+export default contact
