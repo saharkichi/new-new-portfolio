2b602d2d0b6ff615cc6dfd7f4a6dbaea965d705e
saharkichi <saharkichi@gmail.com>
2022-07-25 19:56:21 -0400

    homepage.js updated

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import navbar from './navbar';
 
 function header() {
     return (
-        <navbar/>
+        {navbar}
     )
 }
 
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,1 +1,14 @@
 // WHEN I load the portfolio THEN I am presented with a page containing a header, a section for content, and a footer
+import React from 'react';
+import header from './header';
+import footer from './footer';
+
+function homepage() {
+    return (
+        <p>
+            {[header, footer]}
+        </p>
+    )
+}
+
+export default homepage
