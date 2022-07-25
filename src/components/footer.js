3a8d275aae9a75d5c3ca98043f4b1a849e88ae5b
saharkichi <saharkichi@gmail.com>
2022-07-25 19:41:11 -0400

    footer.js updated & assets folder created

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,12 @@
 //WHEN I view the footer THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) 
+import React from 'react';
+
+function footer() {
+    return (
+        <section alt="GitHub" href="https://github.com/saharkichi" img="./components/assets/download.png"> </section>
+// <div href="https://www.linkedin.com/in/sahar-kichi-414077240/" img="./components/assets/download-1.png" alt="LinkedIn"> </a> </div>
+// need third platform 
+    )
+}
+
+export default footer
