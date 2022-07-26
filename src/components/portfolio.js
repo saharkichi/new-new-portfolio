f1baeda620e2960d4006bf65b19cd8607eaa2836
saharkichi <saharkichi@gmail.com>
2022-07-25 20:09:37 -0400

    portfolio.js updated

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,1 +1,14 @@
 // WHEN I am presented with the Portfolio section THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
+import React from 'react';
+
+function portfolio() {
+    return (
+    <div>
+        <p>
+            filler filler filler 
+        </p>
+    </div>
+    )
+}
+
+export default portfolio
