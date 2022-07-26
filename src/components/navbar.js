02ced54ad28a28668d348d2ea815b58155140a84
saharkichi <saharkichi@gmail.com>
2022-07-25 20:04:09 -0400

    navbar.js updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,1 +1,19 @@
 // WHEN I view the navigation titles THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+import React from 'react';
+import aboutme from './aboutme';
+import portfolio from './portfolio';
+import contact from './contact';
+import resume from './resume';
+
+function navbar() {
+    return (
+    <div>
+        <a href="./aboutme">About Me</a>
+        <a href="./portfolio">Portfolio</a>
+        <a href="./contact">Contact Me</a>
+        <a href="./resume">Resume</a>
+    </div>
+    )
+}
+
+export default navbar
